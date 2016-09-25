54a2d78b3918d6406401de10e5b13b7b851f3ade
ajagana <ajagana@ncsu.edu>
2016-09-25 03:13:59 -0400

    Latency and BW program files added
    Debugging to be done.

---
+++ csc-501-project-1/mem_bw_rw_cycle.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <cpuid.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+/* Code from linux manual */
+uint64_t rdtsc(){
+    unsigned int lo,hi;
+    __asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
+    return ((uint64_t)hi << 32) | lo;
+}
+
+
+int main(int argc, char* argv[])
+{
+	unsigned eax, ebx, ecx, edx, level;
+	eax = 1; /* processor info and feature bits */
+	level = 1;
+    uint64_t i, f;
+
+    int a[1000];
+    FILE* fp;
+    fp = fopen("loop_overhead.txt", "a");
+    // Following code is for write Bandwidth
+    for(int i=0;i<1000;i = i+5){
+        __cpuid(level, eax, ebx, ecx, edx); // for serializing the instructions
+            i = rdtsc();
+
+        srand(time(NULL));
+        a[i] = rand()+5;   
+        a[i+1] = rand();
+        a[i+2] = rand();
+        a[i+3] = rand();
+        a[i+4] = rand();    
+
+        __cpuid(level, eax, ebx, ecx, edx); // for serializing the instructions
+            f = rdtsc();
+          
+        fprintf(fp, "%d\n", (f-i));
+    }
+    fprintf("\n Write BW Ends here \n Read BW Starts here \n");
+    
+    // Following code is for Read Bandwidth
+    int b,c,d,e,f;
+    for(int i=0;i<1000;i = i+5){
+        __cpuid(level, eax, ebx, ecx, edx); // for serializing the instructions
+            i = rdtsc();
+
+        b = a[i] + 5;
+        c = a[i+1] + 10 ;
+        d = a[i+2];
+        e = a[i+3];  
+        f = a[i+4];      
+
+        __cpuid(level, eax, ebx, ecx, edx); // for serializing the instructions
+            f = rdtsc();
+          
+        fprintf(fp, "%d\n", (f-i));
+    }
+    fclose(fp); 
+}
---
+++ csc-501-project-1/mem_bw_rw_time.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <cpuid.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+
+int main(int argc, char* argv[])
+{
+    struct timeval t;
+    time_t start_time;
+    time_t final_time;
+
+    int a[1000];
+    FILE* fp;
+    fp = fopen("loop_overhead.txt", "a");
+
+    // Following code is for write Bandwidth
+    for(int i=0;i<1000;i = i+5){
+        gettimeofday(&t, NULL);
+        start_time = t.tv_usec;
+
+        srand(time(NULL));
+        a[i] = rand()+5;   
+        a[i+1] = rand();
+        a[i+2] = rand();
+        a[i+3] = rand();
+        a[i+4] = rand();
+
+        gettimeofday(&t, NULL);
+        final_time = t.tv_usec;
+
+        fprintf(fp, "%d\n", (final_time-start_time));
+    }
+    fprintf("\n Write BW Ends here \n Read BW Starts here \n");
+    
+    // Following code is for Read Bandwidth
+    int b,c,d,e,f;
+    for(int i=0;i<1000;i = i+5){
+        gettimeofday(&t, NULL);
+        start_time = t.tv_usec;
+        
+        b = a[i] + 5;
+        c = a[i+1] + 10 ;
+        d = a[i+2];
+        e = a[i+3];  
+        f = a[i+4];      
+
+        gettimeofday(&t, NULL);
+        final_time = t.tv_usec;
+
+        fprintf(fp, "%d\n", (final_time-start_time));
+
+    }
+    fclose(fp); 
+}
