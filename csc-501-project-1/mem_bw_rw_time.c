fc5cfd3fe6859e4b67bdc8f1dbd21810d4e1e8b2
ajagana <ajagana@ncsu.edu>
2016-09-25 20:57:18 -0400

    Debugged the code

--- csc-501-project-1/mem_bw_rw_time.c
+++ csc-501-project-1/mem_bw_rw_time.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
-int size = 1000;
+int size = 1024*1024;
 int step = 5;
 
 int main(int argc, char* argv[])
@@ -14,15 +14,15 @@ int main(int argc, char* argv[])
     time_t start_time;
     time_t final_time;
 
-    int a[size];
+    int a[size],j,i;
     FILE* fp;
     fp = fopen("loop_overhead.txt", "a");
 
     // Following code is for write Bandwidth
-    for(int i=0;i<size;i = i+step){
+    for(i=0;i<size;i = i+step){
         gettimeofday(&t, NULL);
         start_time = t.tv_usec;
-        for( int j=0; j<step;j++){
+        for( j=0; j<step;j++){
             srand(time(NULL));
             a[i+j] = rand()+5;
         }
@@ -31,14 +31,14 @@ int main(int argc, char* argv[])
 
         fprintf(fp, "%d\n", (final_time-start_time));
     }
-    fprintf("\n Write BW Ends here \n Read BW Starts here \n");
+    fprintf(fp, "\n Write BW Ends here \n Read BW Starts here \n");
     
     // Following code is for Read Bandwidth
-    int b,c,d,e,f;
-    for(int i=0;i<size;i = i+step){
+    int b;
+    for(i=0;i<size;i = i+step){
         gettimeofday(&t, NULL);
         start_time = t.tv_usec;
-        for( int j=0; j<step;j++){
+        for( j=0; j<step;j++){
             b = a[i+j] + 10;    
         }
         
